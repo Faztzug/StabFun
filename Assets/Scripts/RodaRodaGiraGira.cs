5b7cdfb4cb22af0493f1aeca51c37514143fd923
Faztz Nya <faztzug@gmail.com>
2021-10-19 20:33:33 -0300

    Concertei o bug, facilmente, e agora me sinto burro por ter causado ele em primeiro lugar, mas enfim, com isso toda a lógica básica do jogo está implementada, a partir daqui devemos nos preocupar com implementação de assets e animações.

--- Assets/Scripts/RodaRodaGiraGira.cs
+++ Assets/Scripts/RodaRodaGiraGira.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RodaRodaGiraGira : MonoBehaviour
@@ -17,7 +16,7 @@ public class RodaRodaGiraGira : MonoBehaviour
     [SerializeField] private Vector2 accelarationRange = new Vector2(1, 50);
     [SerializeField] private Vector2 increaseAccelarationRange = new Vector2(0, 1);
     [SerializeField] private Vector2 maxAccelarationRange = new Vector2(10, 50);
-    [SerializeField] private Vector2 timeToChangeRange = new Vector2(0.3f,1.3f);
+    [SerializeField] private Vector2 timeToChangeRange = new Vector2(0.3f, 1.3f);
 
     private Rigidbody2D rgbd2d;
     private Transform cepo;
@@ -53,26 +52,27 @@ public class RodaRodaGiraGira : MonoBehaviour
         {
             dFaca.Jogar();
         }*/
-
     }
-
-    
 
     private void SpawnObjects()
     {
+        int childsNumber;
+        float rng;
+
         for (int i = 0; i < spawnSections; i++)
         {
-            float rng = Random.Range(1f, 100f);
+            rng = Random.Range(1f, 100f);
             transform.rotation = Quaternion.Euler(0, 0, grausSpawn * i);
+            childsNumber = transform.childCount;
 
             if (frutaSpawnChance >= rng)
             {
                 Instantiate(fruta, this.transform);
-                transform.GetChild(i).position = new Vector2(0, 0 + frutaSpawnPosition.y);
+                transform.GetChild(childsNumber).position = new Vector2(0, 0 + frutaSpawnPosition.y);
                 //transform.GetChild(i).Translate(frutaSpawnPosition);
-                transform.GetChild(i).rotation = Quaternion.Euler(0, 0, 180);
+                transform.GetChild(childsNumber).rotation = Quaternion.Euler(0, 0, 180);
                 //transform.GetChild(i).
-                    //RotateAround(transform.GetChild(i).position, this.transform.position, grauAtual);
+                //RotateAround(transform.GetChild(i).position, this.transform.position, grauAtual);
             }
             else
             {
@@ -80,24 +80,19 @@ public class RodaRodaGiraGira : MonoBehaviour
                 if (facaSpawnChance >= rng)
                 {
                     Instantiate(faca, this.transform);
-                    transform.GetChild(i).position = new Vector2(0, 0 + facaSpawnPosition.y);
+                    transform.GetChild(childsNumber).position = new Vector2(0, 0 + facaSpawnPosition.y);
                     //transform.GetChild(i).Translate(frutaSpawnPosition);
-                    transform.GetChild(i).rotation = Quaternion.Euler(0, 0, 180);
+                    transform.GetChild(childsNumber).rotation = Quaternion.Euler(0, 0, 180);
                     //transform.GetChild(i).GetComponent<Faca>().facaBox.enabled = false;
                     contadorFacas.MenosFacas(1);
                 }
             }
 
             //cepo.Rotate(0, 0, grausSpawn);
-            
-            
 
             //grauAtual += grausSpawn;
-
         }
     }
-
-
 
     private void Update()
     {
@@ -106,26 +101,25 @@ public class RodaRodaGiraGira : MonoBehaviour
 
     public void RandomizeStats(bool noStart)
     {
-        sentido = Random.Range(-1f,1f);
-            
-        if(noStart == true)
-        speed = 
-            Random.Range(speedRange.x, speedRange.y);
+        sentido = Random.Range(-1f, 1f);
 
-        maxSpeed = 
-            Random.Range(maxSpeedRange.x, maxSpeedRange.y); 
-        accelaration = 
-            Random.Range(accelarationRange.x, accelarationRange.y); 
-        maxAccelaration = 
-            Random.Range(maxAccelarationRange.x, maxAccelarationRange.y); 
-        increaseAccelaration = 
+        if (noStart == true)
+            speed =
+                Random.Range(speedRange.x, speedRange.y);
+
+        maxSpeed =
+            Random.Range(maxSpeedRange.x, maxSpeedRange.y);
+        accelaration =
+            Random.Range(accelarationRange.x, accelarationRange.y);
+        maxAccelaration =
+            Random.Range(maxAccelarationRange.x, maxAccelarationRange.y);
+        increaseAccelaration =
             Random.Range(increaseAccelarationRange.x, increaseAccelarationRange.y);
-        timeToChange = 
+        timeToChange =
             Random.Range(timeToChangeRange.x, timeToChangeRange.y);
 
         if (sentido <= 0)
             maxSpeed = maxSpeed * -1;
-
     }
 
     public IEnumerator CourotineRandomizeAgain()
@@ -135,7 +129,6 @@ public class RodaRodaGiraGira : MonoBehaviour
         RandomizeStats(false);
 
         StartCoroutine(CourotineRandomizeAgain());
-
     }
 
     public void Rotation()
@@ -174,7 +167,7 @@ public class RodaRodaGiraGira : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Faca"))
         {
-            if (collision.gameObject.GetComponentInParent<Faca>().hit == false 
+            if (collision.gameObject.GetComponentInParent<Faca>().hit == false
                 && collision.gameObject.GetComponentInParent<Faca>().dummyFaca == false)
             {
                 collision.gameObject.transform.SetParent(this.gameObject.transform);
@@ -195,9 +188,7 @@ public class RodaRodaGiraGira : MonoBehaviour
         foreach (Faca faca in GetComponentsInChildren<Faca>())
         {
             faca.LibertarEDestruir();
-            
         }
-
 
         /*int childLenght = transform.childCount;
         for (int i = 0; i <= childLenght; i++)
